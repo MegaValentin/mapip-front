acaec9a2f6d95cd61b8779aa3c13b5fc1436d0f1
MegaValentin <91918490+MegaValentin@users.noreply.github.com>
2025-08-22 13:49:35 -0300

    dashboard responsive

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,46 +1,82 @@
 import { Link } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 import IconLogout from "./icons/LogoutIcon";
+import IpIcon from "./icons/IpIcon";
+import RouterIcon from "./icons/RouterIcon";
+import UsersIcon from "./icons/UsersIcon";
+import TagIcon from "./icons/TagIcon";
+import { useState } from "react";
 
 export default function Dashboard() {
   const { user, isAuthenticated, logout } = useAuth();
+  const [ isOpen, setIsOpen] = useState(false)
 
   if (!isAuthenticated) return null;
 
+  const dataLink = [
+    {to: "/ips", text:"IPS", icon: <IpIcon/> },
+    {to: "/routers", text:"Routers", icon: <RouterIcon/> },
+    {to: "/offices", text:"Gestion de areas", icon: <UsersIcon/> },
+  ]
+
+  const renderLinks = (links) => 
+    links.map((link, index) => (
+      <li className="nav-item" key={index}>
+          <Link to={link.to} className="nav-link ">
+            <strong className="p-2">{link.icon}</strong>{link.text}
+          </Link>
+        </li>
+    ))
   return (
-    <div className=" dashboard" style={{ minWidth: "250px" }}>
-      <Link to="/" >
-      <img src="../../public/logoMapIp.png" alt="Logo" className="dashboard-logo" style={{ maxWidth: "200px" }} />
+    <>
+    {/* Botón hamburguesa SOLO en pantallas chicas */}
+    <button
+      className="btn btn-outline-primary d-md-none m-2"
+      onClick={() => setIsOpen(!isOpen)}
+    >
+      ☰
+    </button>
+
+    <div
+      className={`dashboard  border-end p-3 d-flex flex-column 
+      ${isOpen ? "d-block" : "d-none"} d-md-block`}
+      style={{ minWidth: "250px", height: "100vh" }}
+    >
+      <Link to="/" onClick={() => setIsOpen(false)}>
+        <img
+          src="../../public/logoMapIp.png"
+          alt="Logo"
+          className="dashboard-logo mb-3"
+          style={{ maxWidth: "200px" }}
+        />
       </Link>
+
       <ul className="nav flex-column">
-        <li className="nav-item">
-          <Link to="/ips" className="nav-link ">
-            IPS
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/routers" className="nav-link ">
-            Routers
-          </Link>
-        </li>
-        <li className="nav-item">
-            <Link to="/offices" className="nav-link">
-              Gestion de Areas
-            </Link>
-        </li>
+        {renderLinks(dataLink)}
+
         {user?.role === "admin" && (
           <li className="nav-item">
-            <Link to="/user-management" className="nav-link">
-              Gestion de Usuarios
+            <Link
+              to="/user-management"
+              className="nav-link"
+              onClick={() => setIsOpen(false)}
+            >
+              <strong className="p-2">
+                <TagIcon />
+              </strong>{" "}
+              Usuarios
             </Link>
           </li>
         )}
-        <li className="nav-item">
-                  <button className="nav-link " onClick={logout}>
-                    <IconLogout/>
-                  </button>
-                </li>
+
+        <li className="nav-item mt-auto">
+          <button className="nav-link " onClick={logout}>
+            <strong ><IconLogout /></strong> Cerrar sesión
+          </button>
+        </li>
       </ul>
     </div>
+  </>
+    
   );
 }
